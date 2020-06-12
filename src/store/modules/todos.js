64127f41b0b5d75ecc9673a1e4e30a667fc863e3
jovandosen <jovandosen1994@gmail.com>
2020-06-12 19:29:18 +0200

    todos updated

--- src/store/modules/todos.js
+++ src/store/modules/todos.js
@@ -1,26 +1,20 @@
-// import axios from 'axois'
+import axios from 'axios'
 
 const state = {
-    todos: [
-        {
-            id: 1,
-            title: 'Todo One'
-        },
-        {
-            id: 2,
-            title: 'Todo Two'
-        },
-        {
-            id: 3,
-            title: 'Todo Three'
-        }
-    ]
+    todos: []
 }
 const getters = {
     allTodos: state => state.todos
 }
-const actions = {}
-const mutations = {}
+const actions = {
+    async fetchTodos({ commit }) {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/todos')
+        commit('setTodos', response.data)
+    }
+}
+const mutations = {
+    setTodos: (state, todos) => (state.todos = todos)
+}
 
 export default {
     state,
