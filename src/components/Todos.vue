b5d3e18ab2150cfa5daa11a899be4dce5587e8ce
jovandosen <jovandosen1994@gmail.com>
2020-06-13 15:50:35 +0200

    update todo added

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,8 +1,18 @@
 <template>
     <div>
         <h3>Todos</h3>
+        <div class="legend">
+            <span>Double click to mark as complete</span>
+            <span>
+                <span class="incomplete-box"></span> = Incomplete
+            </span>
+            <span>
+                <span class="complete-box"></span> = Complete
+            </span>
+        </div>
         <div class="todos">
-            <div v-for="todo in allTodos" :key="todo.id" class="todo">
+            <div @dblclick="onDblClock(todo)" v-for="todo in allTodos" :key="todo.id" class="todo" 
+                v-bind:class="{'is-complete': todo.completed}">
                 {{ todo.title }}
                 <i @click="deleteTodo(todo.id)" class="fa fa-trash-o" style="font-size:24px"></i>
             </div>
@@ -15,7 +25,15 @@ import { mapGetters, mapActions } from 'vuex'
 export default {
     name: "Todos",
     methods: {
-        ...mapActions(['fetchTodos', 'deleteTodo'])
+        ...mapActions(['fetchTodos', 'deleteTodo', 'updateTodo']),
+        onDblClock(todo) {
+            const updTodo = {
+                id: todo.id,
+                title: todo.title,
+                completed: !todo.completed
+            }
+            this.updateTodo(updTodo)
+        }
     },
     computed: mapGetters(['allTodos']),
     created() {
@@ -45,5 +63,31 @@ i {
     right: 10px;
     color: #fff;
     cursor: pointer;
+}
+.legend {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 1rem;
+}
+.complete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: #35495e;
+}
+.incomplete-box {
+    display: inline-block;
+    width: 10px;
+    height: 10px;
+    background: #41b883;
+}
+.is-complete {
+    background: #35495e;
+    color: #fff;
+}
+@media (max-width: 500px) {
+    .todos {
+        grid-template-columns: 1fr;
+    }
 }
 </style>
